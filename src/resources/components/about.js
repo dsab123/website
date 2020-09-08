e53b0a4694b56bc771eccbf30e7a33ec8686d896
dsab123 <dsabbaghumd@gmail.com>
2020-09-08 18:46:01 -0400

    fix use of compose with webpack

--- src/resources/components/about.js
+++ src/resources/components/about.js
@@ -4,8 +4,13 @@ import {Blog} from './blog';
 
 @inject(PostApi)
 export class About extends Blog {
-    
+    viewModel;
+
     constructor(PostApi) {
         super(PostApi);
+
+        // super hack since compose doesn't work with webpack?
+        // https://discourse.aurelia.io/t/dynamically-loading-compose-viewmodels-using-webpack/173
+        this.viewModel = './blog';
     }
 }
